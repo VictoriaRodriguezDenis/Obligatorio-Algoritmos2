6f91dae3e4d9c18a7b675feab2575fa3e95befb7
JeroAcosta <ja325942@fi365.ort.edu.uy>
2025-10-05 17:46:13 -0300

    ejercicio 4... terminado?

--- Template/ejercicio4.cpp
+++ Template/ejercicio4.cpp
@@ -3,8 +3,33 @@
 #include "tads\ejercicio4\heapImp.cpp"
 using namespace std;
 
-int main()
-{
-    // TODO
+int main() {
+    int K;
+    cin >> K;
+
+    int** listas = new int*[K];
+    int* tamanos = new int[K];
+
+    for (int i = 0; i < K; i++) {
+        cin >> tamanos[i];
+        listas[i] = new int[tamanos[i]];
+        for (int j = 0; j < tamanos[i]; j++) {
+            cin >> listas[i][j];
+        }
+    }
+
+    int total;
+    int* resultado = fusionarKListas(listas, tamanos, K, total);
+
+    for (int i = 0; i < total; i++) {
+        cout << resultado[i] << "\n";
+    }
+
+    // Liberar memoria
+    delete[] resultado;
+    for (int i = 0; i < K; i++) delete[] listas[i];
+    delete[] listas;
+    delete[] tamanos;
+
     return 0;
 }
--- Template/tads/ejercicio4/heapImp.cpp
+++ Template/tads/ejercicio4/heapImp.cpp
@@ -1,82 +1,112 @@
-#include <string>
 #include <iostream>
 using namespace std;
 
+// Nodo del heap
 class NodoHeap {
 public:
-    int dato;
-    NodoHeap(int dato) : dato(dato) {}
+    int dato;       
+    int idxLista;   // índice de la lista de donde viene
+    int idxElem;    // índice dentro de la lista
+    NodoHeap(int dato, int idxLista, int idxElem) 
+        : dato(dato), idxLista(idxLista), idxElem(idxElem) {}
 };
 
+// Min-Heap
 class repHeap {
 private:
-        NodoHeap** heap;
-        int cant;
-        int tope;
+    NodoHeap** heap;
+    int cant;
+    int tope;
 
-        int getPadre(int pos) { 
-                return (pos - 1) / 2; 
-        }
-        int getHijoIzq(int pos) { 
-                return 2 * pos + 1; 
-        }
-        int getHijoDer(int pos) { 
-                return 2 * pos + 2; 
-        }
+    int getPadre(int pos) { return (pos - 1) / 2; }
+    int getHijoIzq(int pos) { return 2 * pos + 1; }
+    int getHijoDer(int pos) { return 2 * pos + 2; }
 
-        void flotar(NodoHeap** h, int pos) {
-                int posPadre = getPadre(pos);
-                if (pos > 0 && h[pos]->dato < h[posPadre]->dato) {
-                        NodoHeap* aux = h[pos];
-                        h[pos] = h[posPadre];
-                        h[posPadre] = aux;
-                        flotar(h, posPadre);
-                }
+    void flotar(int pos) {
+        int posPadre = getPadre(pos);
+        if (pos > 0 && heap[pos]->dato < heap[posPadre]->dato) {
+            NodoHeap* aux = heap[pos];
+            heap[pos] = heap[posPadre];
+            heap[posPadre] = aux;
+            flotar(posPadre);
         }
+    }
 
-        void hundir(NodoHeap** h, int pos, int cant) {
-                int posCandidato = getHijoIzq(pos);
-                if (posCandidato < cant) {
-                        if (posCandidato + 1 < cant && h[posCandidato + 1]->dato <= h[posCandidato]->dato) {
-                                posCandidato++;
-                        }
-                        if (h[pos]->dato >= h[posCandidato]->dato) {
-                                NodoHeap* aux = h[pos];
-                                h[pos] = h[posCandidato];
-                                h[posCandidato] = aux;
-                                hundir(h, posCandidato, cant);
-                        }
-                }
+    void hundir(int pos) {
+        int posCandidato = getHijoIzq(pos);
+        if (posCandidato < cant) {
+            if (posCandidato + 1 < cant && heap[posCandidato + 1]->dato < heap[posCandidato]->dato) {
+                posCandidato++;
+            }
+            if (heap[pos]->dato > heap[posCandidato]->dato) {
+                NodoHeap* aux = heap[pos];
+                heap[pos] = heap[posCandidato];
+                heap[posCandidato] = aux;
+                hundir(posCandidato);
+            }
         }
+    }
 
 public:
-        repHeap(int tope) {
-                this->tope = tope;
-                this->cant = 0;
-                this->heap = new NodoHeap*[tope];
-                for (int i = 0; i < tope; i++)
-                this->heap[i] = NULL;
-        }
+    repHeap(int tope) {
+        this->tope = tope;
+        this->cant = 0;
+        this->heap = new NodoHeap*[tope];
+        for (int i = 0; i < tope; i++) heap[i] = nullptr;
+    }
 
-        bool esVacia() { 
-                return cant == 0; 
-        }
+    ~repHeap() {
+        for (int i = 0; i < cant; i++) delete heap[i];
+        delete[] heap;
+    }
 
-        int min() { 
-                return heap[0]->dato; 
-        }
+    bool esVacia() { return cant == 0; }
 
-        void encolar(int dato) {
-                heap[cant] = new NodoHeap(dato);
-                flotar(heap, cant++);
-        }
+    void encolar(int dato, int idxLista, int idxElem) {
+        heap[cant] = new NodoHeap(dato, idxLista, idxElem);
+        flotar(cant++);
+    }
 
-        void desencolar() {
-                delete heap[0];
-                heap[0] = heap[--cant];
-                heap[cant] = NULL;
-                hundir(heap, 0, cant);
-        }
+    NodoHeap* minNodo() { return heap[0]; }
+
+    void desencolar() {
+        delete heap[0];
+        heap[0] = heap[--cant];
+        heap[cant] = nullptr;
+        if (cant > 0) hundir(0);
+    }
 };
 
-typedef repHeap* Heap;
+// Función para fusionar K listas
+int* fusionarKListas(int** listas, int* tamanos, int K, int& totalElementos) {
+    totalElementos = 0;
+    for (int i = 0; i < K; i++) totalElementos += tamanos[i];
+
+    int* resultado = new int[totalElementos];
+    int posResultado = 0;
+
+    repHeap heap(totalElementos);
+
+    // Insertar el primer elemento de cada lista
+    for (int i = 0; i < K; i++) {
+        if (tamanos[i] > 0) {
+            heap.encolar(listas[i][0], i, 0);
+        }
+    }
+
+    while (!heap.esVacia()) {
+        NodoHeap* nodo = heap.minNodo();
+        resultado[posResultado++] = nodo->dato;
+        int lista = nodo->idxLista;
+        int elem = nodo->idxElem;
+
+        heap.desencolar();
+
+        // Insertar siguiente elemento de la misma lista
+        if (elem + 1 < tamanos[lista]) {
+            heap.encolar(listas[lista][elem + 1], lista, elem + 1);
+        }
+    }
+
+    return resultado;
+}
