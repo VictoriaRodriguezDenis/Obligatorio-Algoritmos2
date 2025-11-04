ffc2721d1b50a4640cd2d89d397632c255e847cf
JeroAcosta <ja325942@fi365.ort.edu.uy>
2025-11-04 12:18:58 -0300

    El 7 estaba mal, revisar el viernes

--- Template/ejercicio7.cpp
+++ Template/ejercicio7.cpp
@@ -1,42 +1,8 @@
-#include <string>
 #include <iostream>
-#include "tads\ejercicio7\AVLRanking.cpp"
+#include <string>
 using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-
-    string rankingOficial[MAXN];
-    string rankingAyudante[MAXN];
-    int ordenNumerico[MAXN];
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> rankingOficial[i];
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> rankingAyudante[i];
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        ordenNumerico[i] = buscarIndice(rankingOficial, n, rankingAyudante[i]);
-    }
-
-    Nodo *raiz = NULL;
-    int totalInversiones = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        int mayores = 0;
-        raiz = insertar(raiz, ordenNumerico[i], mayores);
-        totalInversiones += mayores;
-    }
-
-    cout << totalInversiones << endl;
     return 0;
 }
