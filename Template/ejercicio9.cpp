e11a7329dc5dec51b1896d520cc0458708bf3532
VictoriaRodriguezDenis <victoriarodriguezdenis@gmail.com>
2025-11-20 12:29:36 -0300

    ej 99

--- Template/ejercicio9.cpp
+++ Template/ejercicio9.cpp
@@ -2,11 +2,27 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/ejercicio9/mochila3D.cpp"
 
 using namespace std;
 
 int main()
 {
-    // TODO
-    return 0;
+    int cantElementos, capacidad, tamano, lineas, puntos;
+    cin >> cantElementos;
+    Elemento** elementos = new Elemento*[cantElementos];
+    for (int i = 0; i < cantElementos; i++)
+    {
+        cin >> tamano >> lineas >> puntos;
+        elementos[i] = new Elemento(tamano, lineas, puntos); 
+    }
+    cin >> capacidad;
+
+    int S, L;
+    cin >> S >> L;
+
+    mochilaDP(elementos, cantElementos, S, L);
+    
+    return 0;    
 }
+
