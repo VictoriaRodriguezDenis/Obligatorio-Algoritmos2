e92898d2e47aa841058255a29b21d094f3ab051e
VictoriaRodriguezDenis <victoriarodriguezdenis@gmail.com>
2025-11-20 12:37:41 -0300

    ult ver

--- Template/tads/ejercicio9/mochila3D.cpp
+++ Template/tads/ejercicio9/mochila3D.cpp
@@ -1,4 +1,4 @@
-/*using namespace std;
+using namespace std;
 #include <iostream>
 
 
@@ -56,56 +56,4 @@ void mochilaDP(Elemento** elementos, int cantElementos, int S, int L){
     }
     
     cout << matDP[cantElementos-1][S][L] << endl;
-}*/
-
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-class Elemento {
-public:
-    int tamano;
-    int lineas;
-    int puntos;
-
-    Elemento(int tamano, int lineas, int puntos)
-        : tamano(tamano), lineas(lineas), puntos(puntos) {}
-};
-
-void mochilaDP(Elemento** elementos, int cantElementos, int S, int L) {
-
-    // dp[i][j][k] = usare los primeros i elementos
-    int*** dp = new int**[cantElementos + 1];
-
-    for (int i = 0; i <= cantElementos; i++) {
-        dp[i] = new int*[S + 1];
-        for (int j = 0; j <= S; j++) {
-            dp[i][j] = new int[L + 1];
-            for (int k = 0; k <= L; k++) {
-                dp[i][j][k] = 0;   // base: 0 elementos ⇒ todo en 0
-            }
-        }
-    }
-
-    for (int i = 1; i <= cantElementos; i++) {
-        Elemento* e = elementos[i - 1];
-        
-        for (int j = 0; j <= S; j++) {
-            for (int k = 0; k <= L; k++) {
-
-                // No lo tomo
-                dp[i][j][k] = dp[i - 1][j][k];
-
-                // Lo tomo (si entra)
-                if (j >= e->tamano && k >= e->lineas) {
-                    dp[i][j][k] = max(
-                        dp[i][j][k],
-                        e->puntos + dp[i - 1][j - e->tamano][k - e->lineas]
-                    );
-                }
-            }
-        }
-    }
-
-    cout << dp[cantElementos][S][L] << endl;
 }
